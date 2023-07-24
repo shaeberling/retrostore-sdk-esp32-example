438fb4fc03ff21bb2bf0f33f86ddceffbb6182c3
Sascha Häberling <mail@sascha-haeberling.com>
2023-07-24 08:48:23 -0700

    Clean-Up: Remove unused CLI and updates comment on main test CLI.

--- main/retrostore_test_main.cpp
+++ main/retrostore_test_main.cpp
@@ -1,11 +1,8 @@
-/* Hello World Example
-
-   This example code is in the Public Domain (or CC0 licensed, at your option.)
-
-   Unless required by applicable law or agreed to in writing, this
-   software is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
-   CONDITIONS OF ANY KIND, either express or implied.
-*/
+/* CLI that uses the retrostore to test functionality.
+ *
+ * This serves both as a test and as documentation on
+ * how to use the API.
+ */
 #include <cstdlib>
 #include <set>
 #include <stdio.h>
